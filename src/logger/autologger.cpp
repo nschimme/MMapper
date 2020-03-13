d7f154de903f6e6fe8e91c7f4cc115cfc70c2d31
Mattias-Viklund <mattetheveo@gmail.com>
2020-03-13 02:26:26 +0100

    Added header

--- src/logger/autologger.cpp
+++ src/logger/autologger.cpp
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright (C) 2019 The MMapper Authors
+// Author: Mattias Viklund <devmew@exedump.com> (Mew_)
+
 #include "autologger.h"
 
 AutoLogger::AutoLogger()

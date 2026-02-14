8b1ff54e61ab215c09fba0664c3bbbbdfbc4b385
nschimme <5505185+nschimme@users.noreply.github.com>
2026-02-14 03:09:35 +0000

    opengl: enable GL_POINT_SPRITE in Compatibility profile
    On many Linux drivers (specifically Mesa), gl_PointCoord in fragment
    shaders is only populated if GL_POINT_SPRITE is enabled when using
    a Compatibility profile. This fix ensures that connection points
    (and other points using gl_PointCoord) are visible on Linux.
    
    Also includes minor formatting fixes in shaders to satisfy clang-format.

--- src/resources/shaders/legacy/point/frag.glsl
+++ src/resources/shaders/legacy/point/frag.glsl
@@ -9,7 +9,7 @@ out vec4 vFragmentColor;
 
 void main()
 {
-    if (dot(gl_PointCoord-0.5, gl_PointCoord-0.5) > 0.25) {
+    if (dot(gl_PointCoord - 0.5, gl_PointCoord - 0.5) > 0.25) {
         discard;
     }
     vFragmentColor = vColor * uColor;

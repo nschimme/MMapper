7d925d4199bc2864a0cd7aa0031b9824a8edf703
Nils Schimmelmann <nschimme@gmail.com>
2026-04-13 10:35:05 -0500

    enable QtKeychain with Wasm

--- src/configuration/PasswordConfig.cpp
+++ src/configuration/PasswordConfig.cpp
@@ -3,11 +3,13 @@
 
 #include "PasswordConfig.h"
 
+#include "../global/ConfigConsts-Computed.h"
+#include "../global/ConfigEnums.h"
 #include "../global/macros.h"
 
 #ifndef MMAPPER_NO_QTKEYCHAIN
-static const QLatin1String PASSWORD_KEY("password");
 static const QLatin1String APP_NAME("org.mume.mmapper");
+static const QLatin1String PASSWORD_KEY("password");
 #endif
 
 PasswordConfig::PasswordConfig(QObject *const parent)
@@ -15,21 +17,43 @@ PasswordConfig::PasswordConfig(QObject *const parent)
 #ifndef MMAPPER_NO_QTKEYCHAIN
     , m_readJob(APP_NAME)
     , m_writeJob(APP_NAME)
+    , m_deleteJob(APP_NAME)
 {
     m_readJob.setAutoDelete(false);
     m_writeJob.setAutoDelete(false);
+    m_deleteJob.setAutoDelete(false);
 
-    connect(&m_readJob, &QKeychain::ReadPasswordJob::finished, [this]() {
+    auto handleError = [this](const QKeychain::Job &job) {
+        if constexpr (CURRENT_PLATFORM == PlatformEnum::Wasm) {
+            if (job.error() == QKeychain::AccessDeniedByUser) {
+                return true;
+            }
+        }
+        emit sig_error(job.errorString());
+        return false;
+    };
+
+    connect(&m_readJob, &QKeychain::ReadPasswordJob::finished, [this, handleError]() {
         if (m_readJob.error()) {
-            emit sig_error(m_readJob.errorString());
+            handleError(m_readJob);
         } else {
             emit sig_incomingPassword(m_readJob.textData());
         }
     });
 
-    connect(&m_writeJob, &QKeychain::WritePasswordJob::finished, [this]() {
+    connect(&m_writeJob, &QKeychain::WritePasswordJob::finished, [this, handleError]() {
         if (m_writeJob.error()) {
-            emit sig_error(m_writeJob.errorString());
+            handleError(m_writeJob);
+        } else {
+            emit sig_passwordSaved();
+        }
+    });
+
+    connect(&m_deleteJob, &QKeychain::DeletePasswordJob::finished, [this, handleError]() {
+        if (m_deleteJob.error()) {
+            handleError(m_deleteJob);
+        } else {
+            emit sig_passwordDeleted();
         }
     });
 }
@@ -38,24 +62,49 @@ PasswordConfig::PasswordConfig(QObject *const parent)
 }
 #endif
 
-void PasswordConfig::setPassword(const QString &password)
+void PasswordConfig::setPassword(const QString &accountName, const QString &password)
 {
 #ifndef MMAPPER_NO_QTKEYCHAIN
-    m_writeJob.setKey(PASSWORD_KEY);
+    if constexpr (CURRENT_PLATFORM == PlatformEnum::Wasm) {
+        m_writeJob.setKey(accountName);
+    } else {
+        m_writeJob.setKey(PASSWORD_KEY);
+    }
     m_writeJob.setTextData(password);
     m_writeJob.start();
 #else
+    std::ignore = accountName;
     std::ignore = password;
     emit sig_error("Password setting is not available.");
 #endif
 }
 
-void PasswordConfig::getPassword()
+void PasswordConfig::getPassword(const QString &accountName)
 {
 #ifndef MMAPPER_NO_QTKEYCHAIN
-    m_readJob.setKey(PASSWORD_KEY);
+    if constexpr (CURRENT_PLATFORM == PlatformEnum::Wasm) {
+        m_readJob.setKey(accountName);
+    } else {
+        m_readJob.setKey(PASSWORD_KEY);
+    }
     m_readJob.start();
 #else
+    std::ignore = accountName;
     emit sig_error("Password retrieval is not available.");
+#endif
+}
+
+void PasswordConfig::deletePassword(const QString &accountName)
+{
+#ifndef MMAPPER_NO_QTKEYCHAIN
+    if constexpr (CURRENT_PLATFORM == PlatformEnum::Wasm) {
+        m_deleteJob.setKey(accountName);
+    } else {
+        m_deleteJob.setKey(PASSWORD_KEY);
+    }
+    m_deleteJob.start();
+#else
+    std::ignore = accountName;
+    emit sig_error("Password deletion is not available.");
 #endif
 }
